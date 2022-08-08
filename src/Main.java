24842c8919c16915d4229559f8abcf0d7e911fda
BiatrizR <bia1romao@gmail.com>
2022-08-08 00:00:51 -0300

    add classes mentorias e cursos

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.dominio.Cursos;
+import br.com.dio.desafio.dominio.Mentorias;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Cursos curso1 = new Cursos();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("Descrição do curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Cursos curso2 = new Cursos();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(6);
+
+        Mentorias mentoria = new Mentorias();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Mentorias.java
@@ -0,0 +1,43 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+//criando a classe
+public class Mentorias {
+    //definindo atributos, com modificadores de acesso(public/private)
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentorias{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
